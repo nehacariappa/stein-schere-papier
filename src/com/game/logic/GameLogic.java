37e333bb3eb8a421fe1776ea1331b1555279c0c2
nehacariappa <neha.cariappa@gmail.com>
2016-01-13 19:00:20 +0100

    Removed Input capture logic

--- src/com/game/logic/GameLogic.java
+++ src/com/game/logic/GameLogic.java
@@ -6,25 +6,19 @@ package com.game.logic;
 // ********************************************* 
 
 
-import java.util.Scanner; 
-
-
 public class GameLogic 
 { 
-	private static Scanner scan;
 
 	public static void main(String[] args)  
 	{ 
-		String player1;//Input  -- "R", "P", or "S" 
+		String player1;
 		String player2 = "R"; //Player2 has always "R"
 		int numberOfGames=0;
 		int iteration = 3;
 		String message;
 
 		CheckConditions checkconditions = new CheckConditions();
-
-		scan = new Scanner(System.in); 
-
+		
 		for(;numberOfGames<=iteration;){
 
 			System.out.println("-----------------------------------------------------------------------------------------");	
@@ -34,10 +28,9 @@ public class GameLogic
 			System.out.println();
 
 
-			//Get Player1's move from the input-- note that this is 
-			// stored as a string 
+			//Get Player1's move from the input
 			System.out.println("Enter your play: "); 
-			player1 = scan.next();
+			player1  =checkconditions.captureInput();
 
 			//Check if the input is valid;if not the player1 is asked to enter 
 			//the input again
